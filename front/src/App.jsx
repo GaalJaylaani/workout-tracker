dd3e627c98a276a2d278c7bae7a261e9fe427f48
NKGuillaume <gnanfack@terpmail.umd.edu>
2026-06-30 21:04:06 -0400

    workout display gird static

---
+++ front/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Workout from './workout.jsx'
+function App() {
+  return (
+    <>
+      <section id="center">
+        
+        <Workout/>
+      </section>
+
+      
+    </>
+  )
+}
+
+export default App
